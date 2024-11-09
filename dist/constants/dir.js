123e137bca0fe9f8bafeb63c3362cbf74a2e70d3
sh <hn4968677@gmail.com>
2024-11-09 23:39:52 +0700

    Thông điệp commit của bạn

--- dist/constants/dir.js
+++ dist/constants/dir.js
@@ -7,7 +7,7 @@ exports.UPLOAD_VIDEO_DIR = exports.UPLOAD_VIDEO_TEMP_DIR = exports.UPLOAD_IMAGE_
 const path_1 = __importDefault(require("path"));
 exports.PATH_ADMIN = '/admin';
 exports.limmit = 7;
-exports.UPLOAD_IMAGE_TEMP_DIR = path_1.default.resolve('src/uploads/images/temp');
-exports.UPLOAD_IMAGE_DIR = path_1.default.resolve('src/uploads/images');
-exports.UPLOAD_VIDEO_TEMP_DIR = path_1.default.resolve('src/uploads/videos/temp');
-exports.UPLOAD_VIDEO_DIR = path_1.default.resolve('src/uploads/videos');
+exports.UPLOAD_IMAGE_TEMP_DIR = path_1.default.resolve('uploads/images/temp');
+exports.UPLOAD_IMAGE_DIR = path_1.default.resolve('uploads/images');
+exports.UPLOAD_VIDEO_TEMP_DIR = path_1.default.resolve('uploads/videos/temp');
+exports.UPLOAD_VIDEO_DIR = path_1.default.resolve('uploads/videos');
